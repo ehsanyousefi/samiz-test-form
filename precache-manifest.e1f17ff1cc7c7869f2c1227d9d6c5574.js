64e43984bd8add09467f23d9182b3c2e2bd88241
Ehsan Yousefi <ehsanyousefidev@gmail.com>
2020-09-09 10:40:07 +0430

    Updates

---
+++ precache-manifest.e1f17ff1cc7c7869f2c1227d9d6c5574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045f8702a67687681ad2e0bbc8583f31",
+    "url": "/ehsanyousefi/samiz-test-form.git/index.html"
+  },
+  {
+    "revision": "4a7c49d8ecfd020a3f96",
+    "url": "/ehsanyousefi/samiz-test-form.git/static/css/main.5dcd9e18.chunk.css"
+  },
+  {
+    "revision": "321fa7949fb390c3b1d9",
+    "url": "/ehsanyousefi/samiz-test-form.git/static/js/2.a0381f6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ehsanyousefi/samiz-test-form.git/static/js/2.a0381f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7c49d8ecfd020a3f96",
+    "url": "/ehsanyousefi/samiz-test-form.git/static/js/main.181780ea.chunk.js"
+  },
+  {
+    "revision": "6151206105a85729c779",
+    "url": "/ehsanyousefi/samiz-test-form.git/static/js/runtime-main.e9cfbde6.js"
+  }
+]);
